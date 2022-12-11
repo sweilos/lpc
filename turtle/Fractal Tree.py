433fc8bec02e40c9ee0964e35bd7d5547fe1ff4f
sweilos <bob17234@gmail.com>
2022-12-11 17:25:35 -0400

    adicionando jogo da tartaruga

--- turtle/Fractal Tree.py
+++ turtle/Fractal Tree.py
@@ -2,9 +2,8 @@ from turtle import *
 
 shape("classic")
 speed('fastest')
-
-
 def draw_fractal(size, level, angle):
+
     if level == 0:
         color("green")
         dot(size)
@@ -13,17 +12,14 @@ def draw_fractal(size, level, angle):
 
     forward(size)
     right(angle)
-
     draw_fractal(size * 0.8, level - 1, angle)
     left(angle * 2)
     draw_fractal(size * 0.8, level - 1, angle)
     right(angle)
     backward(size)
 
-
 left(90)
 color("brown")
-draw_fractal(50, 6, 25)
-
+draw_fractal(70, 5, 25)
 
 mainloop()
