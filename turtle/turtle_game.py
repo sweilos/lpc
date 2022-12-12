f1516fd30ddea93f8bdec2a6d6a22633c6131684
sweilos <bob17234@gmail.com>
2022-12-11 21:44:05 -0400

    correção em relação ao padrão PEP 8

--- turtle/turtle_game.py
+++ turtle/turtle_game.py
@@ -31,26 +31,26 @@ player_two.goto(-200, -100)
 for i in range(20):
 
     if player_one.pos() >= (300, 100):
-        print("Player One Wins!")
+        print("O jogador número 1 ganhou!")
         break
 
     elif player_two.pos() >= (300, -100):
-        print("Player Two Wins!")
+        print("O jogador número 2 ganhou!")
         break
 
     else:
-        player_one_turn = input("Press 'Enter' to roll the die ")
+        player_one_turn = input("Aperte enter para lançar o dado ")
         die_outcome = random.choice(die)
-        print("The result of the die roll is: ")
+        print("O resultado do dado lançado é: ")
         print(die_outcome)
-        print("The number of steps will be: ")
+        print("O número de passos será: ")
         print(20 * die_outcome)
         player_one.fd(20 * die_outcome)
-        player_two_turn = input("Press 'Enter' to roll the die ")
+        player_two_turn = input("Aperte enter para lançar o dado ")
         die_outcome = random.choice(die)
-        print("The result of the die roll is: ")
+        print("O resultado do dado lançado é: ")
         print(die_outcome)
-        print("The number of steps will be: ")
+        print("O número de passos será: ")
         print(20 * die_outcome)
         player_two.fd(20 * die_outcome)
 
