7042079e68392fbfa93e5eb0d2ab02ce017e8485
sweilos <bob17234@gmail.com>
2022-12-16 14:21:09 -0400

    pong refactored

--- atividade 003/pong.py
+++ atividade 003/pong.py
@@ -1,30 +1,30 @@
 import turtle
-import os
+import vlc
 
 # draw screen
 screen = turtle.Screen()
-screen.title("My Pong")
+screen.title("Pong refactored by Yago")
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.tracer(0)
 
-# draw paddle 1
-paddle_1 = turtle.Turtle()
-paddle_1.speed(0)
-paddle_1.shape("square")
-paddle_1.color("white")
-paddle_1.shapesize(stretch_wid=5, stretch_len=1)
-paddle_1.penup()
-paddle_1.goto(-350, 0)
+# draw left paddle
+paddle_l = turtle.Turtle()
+paddle_l.speed(0)
+paddle_l.shape("square")
+paddle_l.color("white")
+paddle_l.shapesize(stretch_wid=5, stretch_len=1)
+paddle_l.penup()
+paddle_l.goto(-350, 0)
 
-# draw paddle 2
-paddle_2 = turtle.Turtle()
-paddle_2.speed(0)
-paddle_2.shape("square")
-paddle_2.color("white")
-paddle_2.shapesize(stretch_wid=5, stretch_len=1)
-paddle_2.penup()
-paddle_2.goto(350, 0)
+# draw right paddle
+paddle_r = turtle.Turtle()
+paddle_r.speed(0)
+paddle_r.shape("square")
+paddle_r.color("white")
+paddle_r.shapesize(stretch_wid=5, stretch_len=1)
+paddle_r.penup()
+paddle_r.goto(350, 0)
 
 # draw ball
 ball = turtle.Turtle()
@@ -33,8 +33,8 @@ ball.shape("square")
 ball.color("white")
 ball.penup()
 ball.goto(0, 0)
-ball.dx = 1
-ball.dy = 1
+ball.dx = 0.2
+ball.dy = -0.2
 
 # score
 score_1 = 0
@@ -51,84 +51,50 @@ hud.goto(0, 260)
 hud.write("0 : 0", align="center", font=("Press Start 2P", 24, "normal"))
 
 
-def paddle_up(paddle):
-    if paddle == paddle_1:
-        y = paddle_1.ycor()
-        if y < 250:
-            y += 30
-        else:
-            y = 250
-        paddle_1.sety(y)
-    else:
-        y = paddle_2.ycor()
-        if y < 250:
-            y += 30
-        else:
-            y = 250
-        paddle_2.sety(y)
-
-
-def paddle_down(paddle):
-    if paddle == paddle_1:
-        y = paddle_1.ycor()
-        if y > -250:
-            y += -30
-        else:
-            y = -250
-        paddle_1.sety(y)
-    else:
-        y = paddle_2.ycor()
-        if y > -250:
-            y += -30
-        else:
-            y = -250
-        paddle_2.sety(y)
-
-
-"""   
-def paddle_1_up():
-    y = paddle_1.ycor()
+def paddle_l_up():
+    y = paddle_l.ycor()
     if y < 250:
-        y += 30
+        y += 20
     else:
         y = 250
-    paddle_1.sety(y)
+    paddle_l.sety(y)
 
 
-def paddle_1_down():
-    y = paddle_1.ycor()
+def paddle_l_down():
+    y = paddle_l.ycor()
     if y > -250:
         y += -30
     else:
         y = -250
-    paddle_1.sety(y)
+    paddle_l.sety(y)
 
 
-def paddle_2_up():
-    y = paddle_2.ycor()
+def paddle_r_up():
+    y = paddle_r.ycor()
     if y < 250:
         y += 30
     else:
         y = 250
-    paddle_2.sety(y)
+    paddle_r.sety(y)
 
 
-def paddle_2_down():
-    y = paddle_2.ycor()
+def paddle_r_down():
+    y = paddle_r.ycor()
     if y > -250:
         y += -30
     else:
         y = -250
-    paddle_2.sety(y)
-"""
+    paddle_r.sety(y)
+
 
 # keyboard
 screen.listen()
-screen.onkeypress(paddle_up("paddle_1"), "w")
-screen.onkeypress(paddle_down("paddle_1"), "s")
-screen.onkeypress(paddle_up("paddle_2"), "Up")
-screen.onkeypress(paddle_down("paddle_2"), "Down")
+screen.onkeypress(paddle_l_up, "w")
+screen.onkeypress(paddle_l_down, "s")
+screen.onkeypress(paddle_r_up, "Up")
+screen.onkeypress(paddle_r_down, "Down")
 
+# game loop
 while True:
     screen.update()
 
@@ -136,42 +102,43 @@ while True:
     ball.setx(ball.xcor() + ball.dx)
     ball.sety(ball.ycor() + ball.dy)
 
-    # collision with the upper wall
+    # ball collision with upper wall
     if ball.ycor() > 290:
-        os.system("afplay bounce.wav&")
         ball.sety(290)
         ball.dy *= -1
+        vlc.MediaPlayer('bounce.wav')
 
-    # collision with lower wall
+    # ball collision with lower wall
     if ball.ycor() < -290:
-        os.system("afplay bounce.wav&")
         ball.sety(-290)
         ball.dy *= -1
+        vlc.MediaPlayer('bounce.wav')
 
-    # collision with left wall
-    if ball.xcor() < -390:
-        score_2 += 1
-        hud.clear()
-        hud.write("{} : {}".format(score_1, score_2), align="center", font=("Press Start 2P", 24, "normal"))
-        os.system("afplay 258020__kodack__arcade-bleep-sound.wav&")
+    # ball collision with right wall
+    if ball.xcor() > 390:
         ball.goto(0, 0)
         ball.dx *= -1
-
-    # collision with right wall
-    if ball.xcor() > 390:
         score_1 += 1
         hud.clear()
         hud.write("{} : {}".format(score_1, score_2), align="center", font=("Press Start 2P", 24, "normal"))
-        os.system("afplay 258020__kodack__arcade-bleep-sound.wav&")
+        vlc.MediaPlayer('bleep_sound.wav')
+
+    # ball collision with left wall
+    if ball.xcor() < -390:
         ball.goto(0, 0)
         ball.dx *= -1
-
-    # collision with the paddle 1
-    if ball.xcor() < -330 and paddle_1.ycor() + 50 > ball.ycor() > paddle_1.ycor() - 50:
+        score_2 += 1
+        hud.clear()
+        hud.write("{} : {}".format(score_1, score_2), align="center", font=("Press Start 2P", 24, "normal"))
+        vlc.MediaPlayer('bleep_sound.wav')
+    # collision with right paddle
+    if (340 < ball.xcor() < 350) and paddle_r.ycor() + 50 > ball.ycor() > paddle_r.ycor() - 50:
+        ball.setx(340)
         ball.dx *= -1
-        os.system("afplay bounce.wav&")
+        vlc.MediaPlayer('bounce.wav')
 
-    # collision with the paddle 2
-    if ball.xcor() > 330 and paddle_2.ycor() + 50 > ball.ycor() > paddle_2.ycor() - 50:
+    # collision with left paddle
+    if (-340 > ball.xcor() > -350) and paddle_l.ycor() + 50 > ball.ycor() > paddle_l.ycor() - 50:
+        ball.setx(-340)
         ball.dx *= -1
-        os.system("afplay bounce.wav&")
+        vlc.MediaPlayer('bounce.wav')
